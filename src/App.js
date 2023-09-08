17670568bb6df6bbd809cea860687d3deedd3364
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-09-08 23:56:59 +0530

    setup(tailwindcss): init setup of tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div className="App">
-      CamiTech Retailer Web App
+    <div className="App text-5xl w-full h-full min-h-screen bg-gray-900 text-gray-100 flex flex-col items-center">
+      <h1 className="mt-96" >
+        CamiTech Retailer Web App
+      </h1>
     </div>
   );
 }
